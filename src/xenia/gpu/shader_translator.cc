588f57b327eb525a89dfff7cd362a72978dcb0f1
Peter van der Veen <p.vanderveen93@gmail.com>
2026-06-01 17:38:58 +0200

    Revert "[GPU] Handle vfetch stride=0 gracefully instead of asserting"
    This reverts commit 4c521ac86d8b2bcbd91f9c8754112e42d61d6962.

--- src/xenia/gpu/shader_translator.cc
+++ src/xenia/gpu/shader_translator.cc
@@ -418,13 +418,7 @@ void Shader::GatherVertexFetchInformation(
     }
   }
   if (!attrib) {
-    if (!fetch_instr.attributes.stride) {
-      XELOGW(
-          "Shader contains a vfetch with stride 0 for fetch constant {}; "
-          "skipping vertex binding (game compatibility issue).",
-          op.fetch_constant_index());
-      return;
-    }
+    assert_not_zero(fetch_instr.attributes.stride);
     VertexBinding vertex_binding;
     vertex_binding.binding_index = int(vertex_bindings_.size());
     vertex_binding.fetch_constant = op.fetch_constant_index();
